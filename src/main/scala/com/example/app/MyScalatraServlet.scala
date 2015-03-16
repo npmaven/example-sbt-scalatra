bb6a3f34db5d98c712e241f93b350fd940305029
Michal Ostruszka <michal.ostruszka@gmail.com>
2015-03-16 19:36:00 +0100

    Add simple mustache view with jQuery usage

--- src/main/scala/com/example/app/MyScalatraServlet.scala
+++ src/main/scala/com/example/app/MyScalatraServlet.scala
@@ -6,19 +6,8 @@ import scalate.ScalateSupport
 class MyScalatraServlet extends ScalatraServlet with ScalateSupport {
 
   get("/") {
-    <html>
-      <body>
-        <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
-      </body>
-    </html>
+    contentType = "text/html"
+    mustache("index", "layout" -> "")
   }
 
-  notFound {
-    // Try to render a ScalateTemplate if no route matched
-    findTemplate(requestPath) map { path =>
-      contentType = "text/html"
-      layoutTemplate(path)
-    } orElse serveStaticResource() getOrElse resourceNotFound() 
-  }
 }
